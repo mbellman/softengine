9a60eeba36590c96207980e013a26fda89f2a293
Zack Kollar <rallokkcaz@gmail.com>
2019-01-28 01:09:13 -0800

    Ooops forgot the constexpr in the CubeVertices definition

--- Source/Objects.h
+++ Source/Objects.h
@@ -33,7 +33,7 @@ struct Mesh : Object {
 };
 
 namespace CubeVertices {
-	const static int vertexMap[12][3] = {
+	constexpr static int vertexMap[12][3] = {
 		{ 0, 1, 4 },
 		{ 1, 4, 5 },
 		{ 1, 2, 5 },
