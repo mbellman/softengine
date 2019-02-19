47191c1969195d6829c44b8406e3acc21f767fcf
Zack Kollar <rallokkcaz@gmail.com>
2019-02-19 01:30:13 -0800

    Initial test of Sound api works

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,5 +1,6 @@
 #include <Engine.h>
 #include <Sound/AudioEngine.h>
+#include <Sound/Sound.h>
 #include <Levels/Default.h>
 #include <Levels/StressTest.h>
 #include <Levels/LightTest.h>
@@ -11,12 +12,16 @@ int height = 720;
 
 int main(int argc, char* argv[]) {
 	AudioEngine audio;
+	Sound* applause = new Sound("./DemoAssets/applause.wav");
 
+	applause->play();
 
 	Engine engine(width, height, DEBUG_STATS);
 
 	engine.setActiveLevel(new Garden());
 	engine.run();
+
+	delete applause;
 
 	return 0;
 }
